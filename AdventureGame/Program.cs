376630882af7c5b75ddee378410fcc76be5d3e24
matthew-oliver777 <matthew.oliver@trojans.dsu.edu>
2020-04-19 12:12:58 -0500

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Cursed Temple of Kumala");
+            Console.WriteLine("Welcome to The Cursed Temple of Kumala, this is a Fantasy Pick Your own adventure game, and my first attempt at making a game! I hope you enjoy it!");
+            Console.Read();
+        }
+    }
+}
